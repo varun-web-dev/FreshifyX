0d8644671c5cb6ab6fb49e1cef1508a67ef074eb
Varun Pal <palvarun8368@gmail.com>
2025-12-11 17:45:25 +0530

    Integrating TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to my React application.</p>
+      <h1 className="bg-red-500 text-5xl">Hello, World!</h1>
+      <p className="bg-amber-500 text-3xl">Welcome to my React application.</p>
     </div>
   );
 }
