455b613ebe93563262473771d60b3e8e555673ca
Varun Pal <palvarun8368@gmail.com>
2025-12-16 19:46:13 +0530

    minor spell mistake

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -5,7 +5,7 @@ import FreshFruits from '../../assets/fresh-fruits.png'
 const Discount = () => {
   return (
     <section className='bg-zinc-200 bg-right bg-contain bg-no-repeat' style={{backgroundImage: `url(${FreshFruits})`}}>
-        <div className='md:flex md:bg-tansparent bg-zinc-200 flex-cols max-w-[1300px] mx-auto px-10 py-10'>
+        <div className='md:flex md:bg-transparent bg-zinc-200 flex-cols max-w-[1300px] mx-auto px-10 py-10'>
             <span className='md:text-9xl text-6xl text-green-500 font-bold transform md:-rotate-90 self-center'>20%</span>
             <div className='max-w-[600px] '>
                 <h3 className='md:text-7xl text-4xl text-zinc-800 font-bold '>First Order Discount!</h3>
